d1ee847c8ed7a4a8e7c42d22d06fbcba32e79953
Cadugonca <carlos22santos03@gmail.com>
2025-07-24 18:53:35 -0300

    feat: adicionar front no github

---
+++ src/sevices/livroService.js
@@ -0,0 +1,40 @@
+
+const API_URL = 'https://apibiblioteca.netlify.app/'
+
+export async function getLivros() {
+  const response = await fetch(API_URL)
+  return response.json()
+}
+
+export async function getLivroPorId(id) {
+  const response = await fetch(`${API_URL}/${id}`)
+  return response.json()
+}
+
+export async function criarLivro(livro) {
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(livro),
+  })
+  return response.json()
+}
+
+export async function editarLivro(id, livro) {
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(livro),
+  })
+  return response.json()
+}
+
+export async function deletarLivro(id) {
+  await fetch(`${API_URL}/${id}`, {
+    method: 'DELETE',
+  })
+}
