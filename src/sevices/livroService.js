bd983191648efe15e7c7253d91694d22c6c7fd0c
Cadugonca <carlos22santos03@gmail.com>
2025-07-24 19:05:47 -0300

    Atualiza URL da API para produção

--- src/sevices/livroService.js
+++ src/sevices/livroService.js
@@ -1,5 +1,5 @@
 
-const API_URL = 'https://apibiblioteca.netlify.app/'
+const API_URL = 'https://apibiblioteca-rmvs.onrender.com/'
 
 export async function getLivros() {
   const response = await fetch(API_URL)
